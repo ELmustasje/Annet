a7a9f1083e9c135e239555b5b7183a0354606b58
ELmustasje <thomasbarth@live.no>
2025-09-11 12:34:35 +0200

    a

---
+++ leetcode/symmetric_tree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+  if (!root) return false;
+  const q = [[root.left, root.right]];
+
+  while (q.length) {
+    const [l, r] = q.shift();
+    if (l == null && r == null) continue;
+    if (l == null || r == null) return false;
+    if (l.val != r.val) return false;
+
+    q.push([l.left, r.right]);
+    q.push([l.right, r.left]);
+  }
+  return true;
+
+};
+
